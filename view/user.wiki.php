cd08a6a0fadcbbe9e15b249674a8c5db274c8f3f
Jérémy Touati <jeremy.touati@gmail.com>
2016-10-01 19:50:56 +0200

    Added Closest Achievements section

--- view/user.wiki.php
+++ view/user.wiki.php
@@ -2,3 +2,9 @@
 
 <h2>Unlocked achievements</h2>
 <AchievementList user="<?php echo $user->getName() ?>" />
+
+<h2>Closest Achievements</h2>
+<?php foreach (PleesherExtension::getClosestAchievements($user->getId(), 3) as $goal): ?>
+<Goal code="<?php echo $goal->code ?>" perspective="<?php echo $user->getName() ?>" />
+<br>
+<?php endforeach ?>
