77d186f5735cb90caca5627190cdc12c014a762a
Jérémy Touati <jeremy.touati@gmail.com>
2016-11-06 15:02:00 +0100

    Achievements' hour on 2 digits

--- view/feed.php
+++ view/feed.php
@@ -1,6 +1,6 @@
 <ul>
 	<?php foreach ($achievements as $achievement): ?>
-	<li><?php echo $achievement->datetime->format('m/d g:i A') ?> - <a href="<?php echo $h->pageUrl('User:' . $achievement->author->getName()) ?>"><?php echo $achievement->author->getName() ?></a>
+	<li><?php echo $achievement->datetime->format('m/d h:i A') ?> - <a href="<?php echo $h->pageUrl('User:' . $achievement->author->getName()) ?>"><?php echo $achievement->author->getName() ?></a>
 	achieved <a href="<?php echo $h->pageUrl('Special:AchievementDetails/' . $achievement->goal->code) ?>"
 	><?php echo $achievement->goal->title ?></a></li>
 	<?php endforeach ?>
