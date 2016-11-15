8acb0f2ee4a4c1644e100b90c5f7e32907d16861
Jérémy Touati <jeremy.touati@gmail.com>
2016-11-15 21:02:05 +0100

    Added error handling to GoalDetailsPage when goal isn't specified in the URL or doesn't exist

--- pages/GoalDetailsPage.php
+++ pages/GoalDetailsPage.php
@@ -8,6 +8,11 @@ class Pleesher_GoalDetailsPage extends SpecialPage
 
 	public function execute($subPage)
 	{
+		$view_helper = new Pleesher_ViewHelper();
+
+		if (empty($subPage))
+			return $this->getOutput()->redirect($view_helper->pageUrl('Special:Achievements'));
+
 		$this->setHeaders();
 		$this->checkPermissions();
 		$this->checkReadOnly();
@@ -18,16 +23,22 @@ class Pleesher_GoalDetailsPage extends SpecialPage
 
 		$user = $this->getUser();
 		$user_id = $user->getId();
-		$goal = PleesherExtension::getGoal($goal_code, ['user_id' => $user_id > 0 ? $user_id : null]);
-		$achievers = PleesherExtension::getAchievers($goal_code);
 
-		$html = PleesherExtension::render('goal_details', [
-			'user' => $user,
-			'goal' => $goal,
-			'achievers' => $achievers
-		]);
+		$goal = null;
+		try {
+			$achievers = PleesherExtension::getAchievers($goal_code);
+			$html = PleesherExtension::render('goal_details', [
+				'user' => $user,
+				'goal' => $goal,
+				'achievers' => $achievers
+			]);
 
-		$this->getOutput()->setPageTitle(wfMessage('pleesher.goal_details.title')->params($goal->title)->inContentLanguage()->escaped());
-		$this->getOutput()->addHTML($html);
+			$this->getOutput()->setPageTitle($view_helper->text('pleesher.goal_details.title', $goal->title));
+			$this->getOutput()->addHTML($html);
+
+		} catch (\Pleesher\Client\Exception\Exception $e) {
+			$this->getOutput()->setPageTitle($view_helper->text('pleesher.error.title'));
+			$this->getOutput()->addHTML($view_helper->text('pleesher.error.text.' . ($e->getErrorCode() ?: 'generic'), $e->getErrorParameters() ?: []));
+		}
 	}
 }
