dc884afa1cfad56bbf97d8eccd43c0aa8a5060e1
Jérémy Touati <jeremy.touati@gmail.com>
2016-11-20 11:28:38 +0100

    Added actionUrl which returns an URL to an api.php action (with optional parameters)

--- ViewHelper.php
+++ ViewHelper.php
@@ -8,6 +8,11 @@ class Pleesher_ViewHelper
 		return $absolute ? $title->getFullURL() : $title->getLocalURL();
 	}
 
+	public function actionUrl($name, array $params = [])
+	{
+		return $GLOBALS['wgScriptPath'] . '/api.php?' . http_build_query(array_merge(['action' => $name], $params));
+	}
+
 	public function text($key, array $params = [])
 	{
 		return wfMessage($key)->params($params)->inContentLanguage()->escaped();
