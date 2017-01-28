b5a7daa8bf627cc6950460c33a3df98a92373292
Jérémy Touati <jeremy.touati@gmail.com>
2017-01-28 17:02:01 +0100

    Added data-confirm handling (JS confirm prompt) + earlier JS handling

--- resources/js/pleesher.js
+++ resources/js/pleesher.js
@@ -1,16 +1,26 @@
-$(function() {
-	$('a[data-redirect]').on('click', function(e) {
+$('a[data-confirm]').on('click', function(e) {
+	if (!confirm($(this).data('confirm'))) {
+		$(this).data('prevented', '1');
 		e.preventDefault();
-		
-		var redirectUrl = $(this).data('redirect');
-		
-		$.get($(this).prop('href')).done(function(result) {
-			if (redirectUrl == 'self') {
-				window.location.reload();
-			}
-			else {
-				window.location = $(this).data('redirect');
-			}
-		});
+	}
+});
+
+$('a[data-redirect]').on('click', function(e) {
+	e.preventDefault();
+	
+	if ($(this).data('prevented') == '1') {
+		$(this).removeData('prevented');
+		return;
+	}
+	
+	var redirectUrl = $(this).data('redirect');
+	
+	$.get($(this).prop('href')).done(function(result) {
+		if (redirectUrl == 'self') {
+			window.location.reload();
+		}
+		else {
+			window.location = $(this).data('redirect');
+		}
 	});
 });
