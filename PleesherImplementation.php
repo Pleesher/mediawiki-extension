22c4e78d45c94a734f2ee0fd01f3e72f96199152
Jérémy Touati <jeremy.touati@gmail.com>
2017-03-28 21:29:48 +0200

    Missing parameter

--- PleesherImplementation.php
+++ PleesherImplementation.php
@@ -12,9 +12,9 @@ abstract class PleesherImplementation
 	public function getLogger()
 	{
 		$logger = new \Monolog\Logger('pleesher');
-		$logger->pushHandler(new \Monolog\Handler\RotatingFileHandler(__DIR__ . '/logs/debug.log', \Monolog\Logger::DEBUG));
-		$logger->pushHandler(new \Monolog\Handler\RotatingFileHandler(__DIR__ . '/logs/warning.log', \Monolog\Logger::WARNING));
-		$logger->pushHandler(new \Monolog\Handler\RotatingFileHandler(__DIR__ . '/logs/error.log', \Monolog\Logger::ERROR));
+		$logger->pushHandler(new \Monolog\Handler\RotatingFileHandler(__DIR__ . '/logs/debug.log', 0, \Monolog\Logger::DEBUG));
+		$logger->pushHandler(new \Monolog\Handler\RotatingFileHandler(__DIR__ . '/logs/warning.log', 0, \Monolog\Logger::WARNING));
+		$logger->pushHandler(new \Monolog\Handler\RotatingFileHandler(__DIR__ . '/logs/error.log', 0, \Monolog\Logger::ERROR));
 
 		return $logger;
 	}
