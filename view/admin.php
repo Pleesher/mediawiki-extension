1d80fbbc9010af5e2f3ffd7f67edc346a9978514
Jérémy Touati <jeremy.touati@gmail.com>
2017-04-09 15:00:43 +0200

    Manage goals action now within a block with description

--- view/admin.php
+++ view/admin.php
@@ -1,11 +1,19 @@
-<p><a target="_blank" href="http://test.develop.pleesher.com/Liquipedia">Manage Liquipedia Goals</a> (ask PoP for usernames/passwords)</p>
+<p></p>
+
+<div class="pleesher-admin-block">
+	<p class="pleesher-admin-block-title"
+		><a target="_blank" href="http://test.develop.pleesher.com/Liquipedia"
+		>Manage Liquipedia Goals</a> - Ask PoP for website account's username/password
+	<p>Use this to add or remove achievements.</p>
+	<p>Checker functions needs to be added to this wiki before new achievements become available. See with PoP or fo-nttax.
+</div>
 
 <div class="pleesher-admin-block">
 	<p class="pleesher-admin-block-title"
 		><a data-redirect="self" data-confirm="Are you sure this is required?"
 		href="<?php echo $h->actionUrl('pleesher.refresh_cache', ['refresh' => 'goals']) ?>"
 		>Refresh goals cache</a></p>
-	Use this after adding/changing a goal from the Pleesher.com admin (see above)
+	Use this after adding/changing a goal from the Pleesher.com admin (see above).
 </div>
 
 <div class="pleesher-admin-block">
@@ -18,7 +26,7 @@
 		<li>The way to check an existing achievement has been changed</li>
 		<li>Something seems wrong with achievements
 	</ul>
-	<strong>This will recompute achievements for all users, and will take quite some time</strong>
+	<p><strong>This will recompute achievements for all users, and will take quite some time.</strong></p>
 </div>
 
 <div class="pleesher-admin-block">
@@ -26,7 +34,7 @@
 		><a data-redirect="self" data-confirm="Are you sure this is required?"
 		href="<?php echo $h->actionUrl('pleesher.refresh_cache', ['refresh' => 'users']) ?>"
 		>Refresh users cache</a></p>
-	Use this if anyone's Kudos seems wrong (in general it should never be used, but it may be useful in case of a bug)
+	Use this if anyone's Kudos seems wrong (in general it should never be used, but it may be useful in case of a bug).
 </div>
 
 <div class="pleesher-admin-block">
@@ -36,5 +44,5 @@
 		>Refresh whole cache</a></p>
 	<p>Use this to remove all cache and fetch/check everything again.</p>
 
-	<strong>Use with caution: this will compute everything from scratch for every user, and will take quite some time</strong>
+	<p><strong>Use with caution: this will compute everything from scratch for every user, and will take quite some time.</strong></p>
 </div>
