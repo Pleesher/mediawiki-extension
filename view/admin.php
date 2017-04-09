88681e72fbc5e95eae37f67166c833d9a1231fff
Jérémy Touati <jeremy.touati@gmail.com>
2017-04-09 15:07:44 +0200

    Split admin page into general/advanced + improved some text

--- view/admin.php
+++ view/admin.php
@@ -1,11 +1,11 @@
-<p></p>
+<h3>General</h3>
 
 <div class="pleesher-admin-block">
 	<p class="pleesher-admin-block-title"
 		><a target="_blank" href="http://test.develop.pleesher.com/Liquipedia"
 		>Manage Liquipedia Goals</a> - Ask PoP for website account's username/password
-	<p>Use this to add or remove achievements.</p>
-	<p>Checker functions needs to be added to this wiki before new achievements become available. See with PoP or fo-nttax.
+	<p>Use this to add or remove goals.</p>
+	<p>Checker functions needs to be added to this wiki before new goals become available. See with PoP or fo-nttax.
 </div>
 
 <div class="pleesher-admin-block">
@@ -16,6 +16,8 @@
 	Use this after adding/changing a goal from the Pleesher.com admin (see above).
 </div>
 
+<h3>Advanced</h3>
+
 <div class="pleesher-admin-block">
 	<p class="pleesher-admin-block-title"
 		><a data-redirect="self" data-confirm="Are you sure this is required?"
@@ -24,7 +26,7 @@
 	Use this only when either:
 	<ul>
 		<li>The way to check an existing achievement has been changed</li>
-		<li>Something seems wrong with achievements
+		<li>Something seems wrong with some people's achievements (progress, status, etc.)</li>
 	</ul>
 	<p><strong>This will recompute achievements for all users, and will take quite some time.</strong></p>
 </div>
