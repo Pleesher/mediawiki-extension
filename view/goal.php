33126eefe9ad11108f1c12ddf1edc984c744188c
Jérémy Touati <jeremy.touati@gmail.com>
2017-04-17 18:17:01 +0200

    Hiding actions when Pleesher is disabled

--- view/goal.php
+++ view/goal.php
@@ -13,7 +13,7 @@
 
 	<?php echo htmlspecialchars($goal->short_description) ?>
 
-	<?php if (isset($user) && $goal->achieved && in_array('revoke', $actions)): ?>
+	<?php if (isset($user) && $goal->achieved && in_array('revoke', $actions) && !PleesherExtension::isDisabled()): ?>
 	<a data-redirect="self" href="<?php echo $h->actionUrl('pleesher.revoke_achievement', ['user_id' => $user->getId(), 'goal_id' => $goal->id, 'duration' => 'forever']) ?>"
 		data-confirm="<?php echo $h->text('pleesher.revoke_achievement.confirm') ?>">Revoke</a>
 	<?php endif ?>
--- view/showcase.php
+++ view/showcase.php
@@ -1,7 +1,7 @@
 <div class="pleesher-showcase">
 	<?php foreach ($goals as $showcased_goal): ?>
 	<div class="pleesher-showcase-item">
-		<div class="pleesher-showcase-item-icon"><?php if ($removable): ?>
+		<div class="pleesher-showcase-item-icon"><?php if ($removable && !PleesherExtension::isDisabled()): ?>
 		[ <a data-redirect="self" href="<?php echo $h->actionUrl('pleesher.showcase_achievement', ['goal_id' => $showcased_goal->id, 'remove' => 1]) ?>">X</a> ]
 		<?php endif ?></div>
 		<div class="pleesher-showcase-item-name"><?php echo $showcased_goal->title ?></div>
