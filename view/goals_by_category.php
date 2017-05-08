dc83f86d82b198b801572547436c62b0adc033c1
Jérémy Touati <jeremy.touati@gmail.com>
2017-05-08 21:26:40 +0200

    Category (text) now fetched from goal data (provided by client)

--- view/goals_by_category.php
+++ view/goals_by_category.php
@@ -1,7 +1,9 @@
 <?php foreach ($goals_by_category as $category => $goals): ?>
-<h2><?php echo $h->text($h->dynPrefix() . '.goal_category.' . $category) ?></h2>
+<h2><?php echo $category ?></h2>
 <?php echo PleesherExtension::render('goals', [
 	'user' => $user,
 	'goals' => $goals
 ]) ?>
 <?php endforeach ?>
+
+<?php echo PleesherExtension::render('_pleesher_ad') ?>
