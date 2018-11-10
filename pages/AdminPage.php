d6f43fcc143288cca8302b6753fa766cfbcb03c7
Jérémy Touati <jeremy.touati@gmail.com>
2018-11-10 17:28:30 +0100

    Sorting users by name (for refresh cache combobox)

--- pages/AdminPage.php
+++ pages/AdminPage.php
@@ -19,6 +19,9 @@ class Pleesher_AdminPage extends Pleesher_SpecialPage
 
 		$pleesher_disabled = PleesherExtension::getSettingValue('disabled', '0') == '1';
 		$users = PleesherExtension::getUsers(['require_achievements_in_cache' => false]);
+		uasort($users, function(User $user1, User $user2) {
+			return strcmp($user1->getName(), $user2->getName());
+		});
 
 		$html = PleesherExtension::render('admin', [
 			'pleesher_disabled' => $pleesher_disabled,
